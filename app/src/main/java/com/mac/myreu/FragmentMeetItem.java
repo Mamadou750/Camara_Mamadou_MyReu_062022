8cf8b044edb2bb0e6c03c7b041b13a53a5695925
camara <camara.mamadou.aliou750@gmail.com>
2022-06-24 13:06:22 +0200

    implementation interface meeting et add meeting sans code

---
+++ app/src/main/java/com/mac/myreu/FragmentMeetItem.java
@@ -0,0 +1,9 @@
+package com.mac.myreu;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
