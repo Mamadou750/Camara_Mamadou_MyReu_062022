cace59818c2cea87bf6751f7ec7c0373347be339
camara <camara.mamadou.aliou750@gmail.com>
2022-06-24 12:50:24 +0200

    implementation data

--- app/src/main/java/com/mac/myreu/data/Meeting.java
+++ app/src/main/java/com/mac/myreu/data/Meeting.java
@@ -1,4 +1,93 @@
 package com.mac.myreu.data;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import java.util.Objects;
+
 public class Meeting {
+    private final long id;
+    @NonNull
+    private final String name;
+    @NonNull
+    private final String roomColor;
+    @Nullable
+    private final String hours;
+    @Nullable
+    private final String roomName;
+    @Nullable
+    private final String mails;
+
+
+    public Meeting(
+            long id,
+            @NonNull String name,
+            @NonNull String roomColor,
+            @Nullable String hours,
+            @Nullable String roomName,
+            @Nullable String mails
+    ) {
+        this.id = id;
+        this.name = name;
+        this.hours = hours;
+        this.roomColor= roomColor;
+        this.roomName= roomName;
+        this.mails = mails;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    @NonNull
+    public String getName() {
+        return name;
+    }
+
+    @Nullable
+    public String getHours() {
+        return hours;
+    }
+
+    @NonNull
+    public String getRoomColor() {
+        return roomColor;
+    }
+
+    @Nullable
+    public String getRoomName() {
+        return roomName;
+    }
+
+    @Nullable
+    public String getMails() {
+        return mails;
+    }
+
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Meeting meeting = (Meeting) o;
+        return id == Meeting.id && name.equals(meeting.name) && Objects.equals(hours, meeting.hours) && Objects.equals(roomName, meeting.roomName) && Objects.equals(roomColor, meeting.roomColor) && Objects.equals(mails ,meeting.mails);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, hours, roomName, roomColor, mails);
+    }
+
+    @NonNull
+    @Override
+    public String toString() {
+        return "Meeting {" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", hours='" + hours + '\'' +
+                ", roomName='" + roomName + '\'' +
+                ", roomColor='" + roomColor + '\'' +
+                ", mails='" + mails+ '\'';
+    }
 }
--- app/src/main/java/com/mac/myreu/data/MeetingRepository.java
+++ app/src/main/java/com/mac/myreu/data/MeetingRepository.java
@@ -1,4 +1,147 @@
 package com.mac.myreu.data;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.Transformations;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 public class MeetingRepository {
+    private final MutableLiveData<List<Meeting>> meetingsLiveData = new MutableLiveData<>(new ArrayList<>());
+
+    private long maxId = 0;
+
+    public MeetingRepository() {
+        // generate meetings
+        generateRandomMeetings();
+
+    }
+
+    public void addMeeting(
+            @NonNull String name,
+            @NonNull String roomColor,
+            @Nullable String roomName,
+            @Nullable String hours,
+            @Nullable String mails
+    ) {
+        List<Meeting> meetings = meetingsLiveData.getValue();
+
+        if (meetings == null) return;
+
+        meetings.add(
+                new Meeting (
+                        maxId++,
+                        name,
+                        roomColor,
+                        roomName,
+                        hours,
+                        mails
+                )
+        );
+
+        meetingsLiveData.setValue(meetings);
+    }
+
+    public void deleteMeeting(long meetingId) {
+        List<Meeting> meetings = meetingsLiveData.getValue();
+
+        if (meetings == null) return;
+
+        for (Iterator<Meeting> iterator = meetings.iterator(); iterator.hasNext(); ) {
+            Meeting meeting = iterator.next();
+
+            if (meeting.getId() == meetingId) {
+                iterator.remove();
+                break;
+            }
+        }
+
+        meetingsLiveData.setValue(meetings);
+    }
+
+    public void toggleFavoriteNeighbour(long id) {
+        List<Meeting> meetings = meetingsLiveData.getValue();
+
+        if (meetings == null) return;
+
+        for (int i = 0; i < meetings.size(); i++) {
+            Meeting meeting = meetings.get(i);
+            if (meeting.getId() == id) {
+                meetings.remove(i);
+                meetings.add(
+                        i,
+                        // This is ugly to copy unchanged fields in Java but immutability is very important !
+                        // Setters are a terrible practice for models outside MVC world.
+                        // Ask about your mentor why immutability of models is so important.
+                        new Meeting(
+                                meeting.getId(),
+                                meeting.getName(),
+                                meeting.getHours(),
+                                meeting.getRoomName(),
+                                meeting.getRoomColor(),
+                                meeting.getMails()
+                        )
+                );
+                break;
+            }
+        }
+
+        meetingsLiveData.setValue(meetings);
+    }
+
+    public LiveData<List<Meeting>> getMeetingsLiveData() {
+        return meetingsLiveData;
+    }
+
+    public LiveData<Meeting> getNeighbourLiveData(long meetingId) {
+        // We use a Transformation here so whenever the neighboursLiveData changes, the underlying lambda will be called too, and
+        // the Neighbour will be re-emitted (with potentially new information like isFavorite set to true or false)
+
+        // This Transformation transforms a List of Neighbours into a Neighbour (matched by its ID)
+        return Transformations.map(meetingsLiveData, meetings -> {
+            for (Meeting meeting : meetings) {
+                if (meeting.getId() == meetingId) {
+                    return meeting;
+                }
+            }
+
+            return null;
+        });
+    }
+
+    private void generateRandomMeetings() {
+        addMeeting(
+                "Peach",
+                "rouge",
+                "Salle 1",
+                "14h",
+                "maxim@lamzom.com, alex@lamzom.com, luc@lamzom.com"
+        );
+        addMeeting(
+                "Jack",
+                "bleu",
+                "Salle 2",
+                "8h",
+                "theo@lamzom.com, sarah@lamzom.com, dan@lamzom.com\""
+        );
+        addMeeting(
+                "Carl",
+                "vert",
+                "Salle 3",
+                "15h",
+                "eric.com, julie@lamzom.com, paul@lamzom.com\""
+        );
+        addMeeting(
+                "Vincent",
+                "gris",
+                "Salle 4",
+                "18h",
+                "kara@lamzom.com, lara@lamzom.com, dexter@lamzom.com\""
+        );
+
+    }
 }
